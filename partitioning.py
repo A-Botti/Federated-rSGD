e327d178e0130c560a7d970c2514a5d89b426020
A-Botti <62095392+A-Botti@users.noreply.github.com>
2022-02-13 15:04:40 +0100

    Starting pack

---
+++ partitioning.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+# Partitioning functions to divide/distribute 'local' datasets
+def iid_partition(dataset, n_clients):
+    """
+    Creating an I.I.D. parition of data to then distribute between clients (i.e. simulating local datasets)
+
+    params:
+    - dataset (torch.utils.Dataset):   dataset instance
+    - n_clients (int):                 number of Clients between which to split the data
+
+    return: Dict of image indexes for each client
+    """
+    num_items_per_client = int(len(dataset)/n_clients)
+    client_dict = {}
+    image_idxs = np.arange(len(dataset))
+
+    # assigning different images to each client
+    for cl in range(n_clients):
+        client_dict[cl] = set(np.random.choice(image_idxs, 
+                                              num_items_per_client, 
+                                              replace=False))
+        image_idxs = list(set(image_idxs) - client_dict[cl])
+    return client_dict
+
+
+def non_iid_partition(dataset, n_clients, num_shards_per_client):
+    """
+    Creating a NON-I.I.D paritition of data over clients (in the paper, each client has 2 shards)
+
+    params:
+    - dataset (torch.utils.Dataset):  dataset from source
+    - n_clients (int):                number of Clients between which to split the data
+    - num_shards_per_client (int):    Number of shards to assign each client
+
+    returns: Dict of image indexes for each client
+    """
+
+    client_dict  = {i: np.array([], dtype='int64') for i in range(n_clients)}
+    total_shards = n_clients * num_shards_per_client
+    shards_size  = int(len(dataset)/total_shards)
+    shard_idxs   = np.arange(total_shards)
+    idxs         = np.arange(len(dataset))
+    data_labels  = dataset.targets # .numpy()
+
+    # sort the labels
+    label_idxs = np.vstack((idxs, data_labels))
+    label_idxs = label_idxs[:, label_idxs[1,:].argsort()]
+    idxs = label_idxs[0,:]
+
+    # assign num_shards_per_client to each client
+    for i in range(n_clients):
+        i_set = set(np.random.choice(shard_idxs,
+                                     num_shards_per_client, 
+                                     replace=False))
+        shard_idxs = list(set(shard_idxs) - i_set)
+
+        for el in i_set:
+            client_dict[i] = np.concatenate(
+                (client_dict[i], idxs[el*shards_size:(el+1)*shards_size]), axis=0)
+  
+    return client_dict
---
+++ utils.py
@@ -0,0 +1,93 @@
+import copy
+import torch
+from torch.utils.data import DataLoader
+from torchvision import transforms, utils, datasets
+
+
+def get_dataset(dataset):
+    """
+    Retrieve the selected dataset train and test set 
+    
+    Param: dataset (string) : either 'mnist' or 'cifar10'
+    returns: train and test splits from source
+    """
+    
+    if dataset == "mnist":
+
+        # Same transform used for train and test
+        transform = transforms.Compose([#transforms.Pad(2),
+                                        transforms.ToTensor(),
+                                        transforms.Normalize(mean = (0.1307,),
+                                                             std = (0.3081,))
+                                      ])
+
+        # will download dataset if not already downloaded; meanwhile applying the transformations
+        trainset = datasets.MNIST('./data/mnist/', train=True, download=True, transform=transform)
+        testset = datasets.MNIST('../data/mnist/', train=False, download=True, transform=transform)
+        
+        
+    elif dataset == "cifar10":
+        
+        # Different transform for train & test, we do some augmentation on training
+        transform_train = transforms.Compose([transforms.RandomCrop(size = (32,32), padding = 4),
+                                              transforms.RandomHorizontalFlip(),
+                                              transforms.ToTensor(),
+                                              transforms.Normalize(mean = (0.485, 0.456, 0.406),
+                                                                   std = (0.229, 0.224, 0.225))
+                                      ])
+        transform_test = transforms.Compose([transforms.ToTensor(),
+                                             transforms.Normalize(mean = (0.485, 0.456, 0.406),
+                                                                  std = (0.229, 0.224, 0.225))
+                                      ])
+        # Download CIFAR10 dateset Training and Test data while applying transforms
+        trainset = datasets.CIFAR10('./data/cifar/', train=True, download=True, transform=transform_train)
+        testset = datasets.CIFAR10('../data/cifar/', train=False, download=True, transform=transform_test)
+        
+    return trainset, testset
+
+def server_aggregate(w):
+    """
+    Updating the global weights by averaging client's results
+    Params:
+    - w (list of model.state_dict()): list of the k clients' model parmeters that are sent to the server at each communication round
+
+    Returns: server updated weights
+    """
+    weights_avg = copy.deepcopy(w[0])
+    for k in weights_avg.keys():
+        weights_avg[k] = torch.stack([w[i][k] for i in range(len(w))], 0).mean(0)
+    
+    return weights_avg # round aggregated/averaged parameters
+
+
+def test_metrics(model, test_ds, criterion, e_batchSize):
+    """
+    Function to evaluate model's performance accuracy. Will be called at every communication round
+
+    Params:
+    - model (nn.Module):       instance of the NN
+    - test_ds (nn.Dataset):    Dataset used for evaluation
+    - criterion (nn.Loss):     Loss function to evaluate
+
+    returns: test set loss and accuracy
+    """
+
+    model.eval()                  # set model in evaluation mode
+    loss, correct = 0.0, 0.0      # placeholder
+
+    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+    test_loader = DataLoader(test_ds, batch_size=e_batchSize)
+
+    # inference and prediction for each batch
+    for data, labels in test_loader:      
+        data, labels = data.to(device), labels.to(device)
+
+        output = model(data)
+        loss += criterion(output, labels).item()                #sum up batch loss
+        pred = output.argmax(dim=1, keepdim=True)               #get predicted class index
+        correct += pred.eq(labels.view_as(pred)).sum().item()
+
+    loss /= len(test_loader.dataset)
+    accuracy = correct/len(test_loader.dataset)
+    
+    return loss, accuracy
